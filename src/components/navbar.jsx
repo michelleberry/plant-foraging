bfd93358b1404dc47895337f996db0a4d01a33e3
Michelle Berry <mberry05@uoguelph.ca>
2021-11-06 16:38:48 -0400

    rearrange navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import{Link} from 'react-router-dom';
+import logo from './logo.png';
 import {
   Nav, NavDropdown, Navbar, Container,FormControl, Form, Button
 } from 'react-bootstrap';
 
 export default function Mynavbar() {
+  var icon = (
+    <span class="logo">
+      <a href="/">
+        <img src={logo} height="33" width="120" alt="text here" /></a>
+    </span>
+  );
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar brand={icon} bg="light" expand="lg">
         <Container>
-            <Navbar.Brand as={Link} to='/'>PLAAAANTS</Navbar.Brand>
+            
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
+            
             <Nav className="me-auto">
                 <Nav.Link as={Link} to='about'>About</Nav.Link>
                 <Nav.Link as={Link} to='social'>Social Media</Nav.Link>
                 <Nav.Link as={Link} to='plants'>Plant Database</Nav.Link>
-               
+            </Nav>
+            <Navbar.Brand as={Link} to='/'>Shrub</Navbar.Brand>
+            <img src={logo} className="App-logo-small" alt="logo" />
+            <Navbar.Brand as={Link} to='/'>Shack</Navbar.Brand>
+            <Nav className="me-auto">
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
