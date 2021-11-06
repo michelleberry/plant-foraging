eb2bf44afea1a27ba1b1d66e375243ef78b1e8f8
Michelle Berry <mberry05@uoguelph.ca>
2021-11-06 14:06:58 -0400

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './plant.jfif';
 import './App.css';
+import Mynavbar from './navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Mynavbar></Mynavbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>PLANT FORAGING!</h1>
