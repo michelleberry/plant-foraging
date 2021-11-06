ae50ee84c9071fdfcd3604d994cd5848f64f0ea3
Rachael Mohl <rmohl@uoguelph.ca>
2021-11-06 19:30:57 -0400

    adding css to about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './about.css';
 
 export default function About() {
    return (
       <div>
-         <h2>About Us</h2>
-         <h5>We are a collaborative created to help people find sources for food by foraging! 
+         <h2>ABOUT US</h2>
+         <p>We are a collaborative created to help people find sources for food by foraging! 
              Foraging is a resourceful and sustainable way to live but information on what is safe to eat can 
              be difficult to gather which is why we created Savoury Shrubs. Our collaborative 
-             helps individuals on their quest to find local and safe sources of food near them.</h5> 
-         <h2>Mission Statement</h2>
-         <h4>We are striving to teach the community about an alternative way to source food, foraging and to educate the population on the correct ways to forage and consume plants.</h4> 
+             helps individuals on their quest to find local and safe sources of food near them.</p> 
+         <h2>MISSION STATEMENT</h2>
+         <p>We are striving to teach the community about an alternative way to source food by foraging and to educate the population on the correct ways to forage and consume plants.</p> 
       </div>
    );
 }
